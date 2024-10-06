1485727d28653d8d384eb8e196946ee5b632a796
Parth Vyas <itspv7@gmail.com>
2024-10-06 18:07:04 +0530

    Completed controller

--- backend/controller.js
+++ backend/controller.js
@@ -20,5 +20,44 @@ const getAllPosts = async (req, res) => {
   }
 };
 
+const getPost = async (req, res) => {
+  const { id } = req.params;
 
+  try {
+    const post = await Post.findById(id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(400).json({ message: 'Error getting post', error });
+  }
+}
 
+const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+
+  try {
+    const post = await Post.findByIdAndUpdate(id, { title, content });
+    res.status(200).json({ message: 'Post updated!' });
+  } catch (error) {
+    res.status(400).json({ message: 'Error updating post', error });
+  }
+};
+
+const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await Post.findByIdAndDelete(id);
+    res.status(200).json({ message: 'Post deleted!' });
+  } catch (error) {
+    res.status(400).json({ message: 'Error deleting post', error });
+  }
+};
+
+module.exports = {
+  createPost,
+  getAllPosts,
+  getPost,
+  updatePost,
+  deletePost,
+};
